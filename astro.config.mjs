e46cda31f9b9c53259b79f496970dc4a31e35f1d
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2024-10-11 15:48:26 +0800

    style: change badge color according to tool

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://farhanah-izzati.netlify.app",
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), icon()],
 });
--- src/pages/aboutMeDetails.ts
+++ src/pages/aboutMeDetails.ts
@@ -1,18 +1,18 @@
 export const aboutMe = [
   {
-    icon: "terminal-window",
+    icon: "mdi:terminal",
     trait: "Aspiring Data Analyst",
     description:
       "Driven by a passion for uncovering insights in numbers and committed to continuous learning and growth",
   },
   {
-    icon: "strategy",
+    icon: "mdi:strategy",
     trait: "Analytical Mindset",
     description:
       "Leveraging strong analytical skills and curiosity to excel in data analysis, with a focus on extracting, cleaning, and visualizing data",
   },
   {
-    icon: "trophy",
+    icon: "mdi:trophy",
     trait: "Data Enthusiast",
     description:
       "Dedicated to contributing to projects that reveal patterns, trends, and opportunities through data exploration and analysis",
